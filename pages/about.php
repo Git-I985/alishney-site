2ea66de8a4ed3281830fae6116ddbd1b9b54d4e9
EdwardKonovalov <edk@primexbt.ch>
2022-02-07 02:13:59 +0300

    added contact and about pages

--- pages/about.php
+++ pages/about.php
@@ -1,1 +1,37 @@
-<h1>About</h1>
+<section class="about-banner">
+    <div class="about-banner-content">
+        <h1>Чем занимается AlishneyCosmetic?</h1>
+    </div>
+</section>
+
+<section class="about-main">
+    <div class="about-main-gallery">
+        <div class="row">
+            <div class="column">
+                <img src="./images/about-page-gallery/1.jpeg" alt="">
+                <img src="./images/about-page-gallery/2.jpg" alt="">
+                <img src="./images/about-page-gallery/3.jpg" alt="">
+            </div>
+            <div class="column">
+                <img src="./images/about-page-gallery/4.jpg" alt="">
+                <img src="./images/about-page-gallery/5.jpg" alt="">
+            </div>
+        </div>
+
+    </div>
+    <div class="about-main-content">
+        <h2>Мы творим для вас.</h2>
+        <p>AlishneyCosmetics - это высококлассная косметическая компания, расположенная в самом центре Нью-Йорка. Каждое из
+            ваших косметических средств изготовлено вручную и не содержит насекомых, является  наихудшей
+            из возможной косметики. Мы учитываем мнение каждого из наших клиентов и ценим этот вклад в процессе
+            производства и без него конечно.
+            Сотрудничая с различными розничными торговцами товарами по всему Нью-Йорку, наши товары доступны в магазинах Sephora,
+            Target, Ultra и небольших будках. Будучи небольшой индивидуальной компанией, принадлежащей Мэдисон Янг, мы
+            ценим каждое из наших партнерских отношений и подставляем самую передовую косметику. МОЙ макияж, отмеченный
+            наградами в области косметики, был показан в таких журналах, как VOGUE, Glamour, Cosmopolitan, allure,
+            beautyscoop и BScene.
+            Мы продолжаем каждый день создавать новые идеи, которые меняют облик индустрии макияжа и совершают революцию
+            в мире красоты. Следите за нами в социальных сетях MY Cosmetics или подпишитесь на нашу ежемесячную посылку
+            новостей.</p>
+    </div>
+</section>
--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,14 @@
-<h1>Contact</h1>
+<section class="contacts-banner">
+    <div class="contacts-banner-content">
+        <div class="map-card">
+            <div class="map-card-header">
+                <a href="mailto:contact@alishney.cosmetic.com"><i class="far fa-envelope"></i>contact@alishney.cosmetic.com</a>
+            </div>
+            <div class="map-card-body">
+                <h1>AlishneyCosmetic Showroom</h1>
+                Пресненская набережная, 12<br>
+                Москва, Россия, 123112
+            </div>
+        </div>
+    </div>
+</section>
