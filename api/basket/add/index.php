f87157d6e6afd794ad21b1e97fd663b37d2bc44e
EdwardKonovalov <edk@primexbt.ch>
2022-02-06 18:22:57 +0300

    Added api error handling

--- api/basket/add/index.php
+++ api/basket/add/index.php
@@ -1,17 +1,21 @@
 <?php
 
-include "../../../utils.php";
-include "../../../data.php";
+try {
+    include "../../../utils.php";
+    include "../../../data.php";
 
-session_start();
+    session_start();
 
-if ( ! isset($_SESSION['basket'])) {
-    $_SESSION['basket'] = [];
+    if ( ! isset($_SESSION['basket'])) {
+        $_SESSION['basket'] = [];
+    }
+
+    $url_parts = explode('/' , $_SERVER['REQUEST_URI']);
+    $requestItemId = (int) $url_parts[count($url_parts) - 1];
+
+
+    $_SESSION['basket'][] = $requestItemId;
+    echo json_encode(["basket_item" => getItemById($requestItemId, $makeup)]);
+} catch (Exception $e) {
+    echo json_encode(["error" => $e]);
 }
-
-$url_parts = explode('/' , $_SERVER['REQUEST_URI']);
-$requestItemId = (int) $url_parts[count($url_parts) - 1];
-
-
-$_SESSION['basket'][] = $requestItemId;
-echo json_encode(["basket_item" => getItemById($requestItemId, $makeup)]);
